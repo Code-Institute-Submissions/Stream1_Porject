3a57042445b82417651deeec8b1efd7c917525bc
Rebecca Schrader <rmschrader1994@gmail.com>
2018-04-24 13:19:18 +0000

    Javascript Validated

--- JS/quiz3.js
+++ JS/quiz3.js
@@ -1,15 +1,15 @@
 function check0(){
     var score = 0;
-    if (document.forms["quiz1"]["question1"].value == "Mark Hamill") {
+    if (document.forms.quiz1.question1.value == "Mark Hamill") {
         score++;}
-    if (document.forms["quiz1"]["question2"].value == "New Orleans") {
+    if (document.forms.quiz1.question2.value == "New Orleans") {
         score++;}
-    if (document.getElementById("numb").value == 1700) {
+    if (document.getElementById("answer3").value == 1700) {
         score++;
     }
-    if (document.forms["quiz1"]["question4"].value == "Gunther") {
+    if (document.forms.quiz1.question4.value == "Gunther") {
         score++;}
-    if (document.forms["quiz1"]["question5"].value == "People's Republic of Benin") {
+    if (document.forms.quiz1.question5.value == "People's Republic of Benin") {
         score++;}
     document.getElementById("demo").innerHTML = score+"/5";
     alert("You finished the Sins of the Father quiz, you should try the other quizzes!");
@@ -18,17 +18,17 @@ function check0(){
 
 function check1(){
     var score = 0;
-    if (document.forms["quiz2"]["question_2.1"].value == "Kay E. Kuter") {
+    if (document.forms.quiz2.question6.value == "Kay E. Kuter") {
         score++;}
-    if (document.forms["quiz2"]["question_2.2"].value == "Smith") {
+    if (document.forms.quiz2.question7.value == "Smith") {
         score++;}
-    if (document.getElementById("numb2").value == 1738) {
+    if (document.getElementById("answer8").value == 1738) {
         score++;}
-    if (document.forms["quiz2"]["question_2.4"].value == "Garr von Zell") {
+    if (document.forms.quiz2.question9.value == "Garr von Zell") {
         score++;}
     
-   var a4 = document.getElementsByName('question_2.5');
-   var a4answers = new Array();
+   var a4 = document.getElementsByName('answer10');
+   var a4answers = [];
    var a4right = new Array('Seeshaupt','Neuschwanstein');
    var a4bool = true;
  
@@ -60,17 +60,17 @@ function check1(){
 
 function check2(){
     var score = 0;
-    if (document.forms["quiz3"]["question_3.1"].value == "Illuminati") {
+    if (document.forms.quiz3.question11.value == "Illuminati") {
         score++;}
-    if (document.forms["quiz3"]["question_3.2"].value == "Albany") {
+    if (document.forms.quiz3.question12.value == "Albany") {
         score++;}
-    if (document.forms["quiz3"]["question_3.3"].value == "Chateau") {
+    if (document.forms.quiz3.question13.value == "Chateau") {
         score++;}
-    if (document.forms["quiz3"]["question_3.4"].value == "Yes") {
+    if (document.forms.quiz3.question14.value == "Yes") {
         score++;}
     
-    var a4 = document.getElementsByName('question_3.5');
-   var a4answers = new Array();
+    var a4 = document.getElementsByName('answer15');
+   var a4answers = [];
    var a4right = new Array('Rome','Naples');
    var a4bool = true;
  
